8d20f4c3d2d363ff52e30086e307ed6296eba9e5
Leah Migacz <lmigacz@msudenver.edu>
2020-04-21 18:47:40 -0600

    added functions

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ mongoose.Promise = global.Promise;
 const db = mongoose.connect('mongodb://localhost:27017/customer_db', {
     useMongoClient: true
 });
+
+const Customer = require('./models/customer');
+
+// add a customer to db
+const addCustomer = (customer) => {
+    Customer.create(customer).then(customer => {
+        console.info('New customer added');
+        db.close();
+    })
+}
+
+// find a customer
+const findCustomer = (name) => {
+    // specify to make it not case sensitive 
+    const search = RegExp(name, 'i');
+    Customer.find({$or: [{firstName: search}, {lastName: search}]})
+    .then(customer => {
+        console.infp(customer);
+        console.info(`${customer.length} matches`);
+        db.close();
+    })
+}
